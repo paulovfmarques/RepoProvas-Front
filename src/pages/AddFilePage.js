533d7f58d1ed65ea557b3012039706151d0e97d2
Paulo <marquespaulov@gmail.com>
2020-12-18 16:44:25 -0300

    vercel deployment

--- src/pages/AddFilePage.js
+++ src/pages/AddFilePage.js
@@ -36,6 +36,7 @@ export default function AddFilePage() {
                         loading = {loading}
                         setLoading = {setLoading}
                         setUploadMsg = {setUploadMsg}
+                        
                     />
                 )}
                 
